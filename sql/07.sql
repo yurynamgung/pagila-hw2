c319744b57a4ca37bd67fb2bc9230b0e469a2680
Mike Izbicki <mike@izbicki.me>
2021-02-26 10:17:39 -0800

    clarify 07

--- sql/07.sql
+++ sql/07.sql
@@ -2,6 +2,10 @@
  * A group of social scientists is studying American movie watching habits.
  * To help them, select the titles of all films that have never been rented by someone who lives in the United States.
  *
+ * NOTE:
+ * Not every film in the film table is available in the store's inventory,
+ * and you should only return films in the inventory.
+ *
  * HINT:
  * This can be solved either with a LEFT JOIN or with the NOT IN operator.
  * You may choose whichever solution makes the most sense to you.
